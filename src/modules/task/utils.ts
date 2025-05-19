938e4ce4d9611a18dc1583b53b59116633794af4
agxmeister <agxmeister@gmail.com>
2025-05-19 19:23:49 +0300

    Exclude official holidays from the tasks' timelines.

--- src/modules/task/utils.ts
+++ src/modules/task/utils.ts
@@ -1,4 +1,4 @@
-import {eachDayOfInterval, isWeekend} from "date-fns";
+import {eachDayOfInterval, isWeekend, format} from "date-fns";
 import {Task} from "@/modules/task/types";
 
 export const refineJiraData = (data: any): Task => ({
@@ -11,5 +11,12 @@ export const getWorkdaysDiff = (start: string, end: string) => {
         start: new Date(start),
         end: new Date(end),
     });
-    return days.filter(day => !isWeekend(day)).length;
+    const holidays = [
+        "2025-01-01", "2025-03-03", "2025-04-18", "2025-04-21", "2025-05-01", "2025-05-06", "2025-05-26",
+        "2025-09-08", "2025-09-22", "2025-21-24", "2025-12-25", "2025-12-26",
+    ];
+    return days
+        .filter(day => !isWeekend(day))
+        .filter(day => !holidays.includes(format(day, 'yyyy-MM-dd')))
+        .length;
 }
